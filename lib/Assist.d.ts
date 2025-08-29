9d48640ba4188c55eaf5383ad7e43c5cff19ee71
Vincent Chen <vincent.chen@aven.com>
2025-08-28 17:23:48 -0700

    build

--- lib/Assist.d.ts
+++ lib/Assist.d.ts
@@ -1,23 +1,11 @@
-import type { Properties } from 'csstype';
 import { App } from '@openreplay/tracker';
-import type { Options as ConfirmOptions } from './ConfirmWindow/defaults.js';
 type StartEndCallback = (agentInfo?: Record<string, any>) => ((() => any) | void);
 export interface Options {
     onAgentConnect: StartEndCallback;
-    onCallStart: StartEndCallback;
     onRecordingRequest?: (agentInfo: Record<string, any>) => any;
-    onCallDeny?: () => any;
     onRecordingDeny?: (agentInfo: Record<string, any>) => any;
-    session_calling_peer_key: string;
-    session_control_peer_key: string;
-    callConfirm: ConfirmOptions;
-    controlConfirm: ConfirmOptions;
-    recordingConfirm: ConfirmOptions;
-    confirmText?: string;
-    confirmStyle?: Properties;
-    config: RTCConfiguration;
+    recordingConfirm: any;
     serverURL: string;
-    callUITemplate?: string;
 }
 export default class Assist {
     private readonly app;
@@ -26,17 +14,14 @@ export default class Assist {
     private socket;
     private peer;
     private assistDemandedRestart;
-    private callingState;
     private agents;
     private readonly options;
     constructor(app: App, options?: Partial<Options>, noSecureMode?: boolean);
     private emit;
     private get agentsConnected();
-    private readonly setCallingState;
     private getHost;
     private getBasePrefixUrl;
     private onStart;
-    private playNotificationSound;
     private clean;
 }
 export {};
