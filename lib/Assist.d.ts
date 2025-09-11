9e7517e09b80edd3adb98ec3f995dcb1e069cbc1
Vincent Chen <vincent.chen@aven.com>
2025-09-11 16:31:35 -0700

    Revert "remove extraneous sockets"
    This reverts commit 030687243a44aa6bb22a2fa94103767b08e0140a.

--- lib/Assist.d.ts
+++ lib/Assist.d.ts
@@ -1,5 +1,7 @@
 import { App } from '@openreplay/tracker';
+type StartEndCallback = (agentInfo?: Record<string, any>) => ((() => any) | void);
 export interface Options {
+    onAgentConnect: StartEndCallback;
     onRecordingRequest?: (agentInfo: Record<string, any>) => any;
     onRecordingDeny?: (agentInfo: Record<string, any>) => any;
     recordingConfirm: any;
@@ -11,11 +13,14 @@ export default class Assist {
     readonly version = "6.0.0";
     private socket;
     private assistDemandedRestart;
+    private agents;
     private readonly options;
     constructor(app: App, options?: Partial<Options>, noSecureMode?: boolean);
     private emit;
+    private get agentsConnected();
     private getHost;
     private getBasePrefixUrl;
     private onStart;
     private clean;
 }
+export {};
